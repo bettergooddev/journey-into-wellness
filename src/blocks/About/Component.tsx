4f0c94b21d1a04465be6f6dde6ec04cdcdea8b4a
bettergooddev <dev@bettergood.agency>
2025-03-24 17:44:20 -0400

    making components

--- src/blocks/About/Component.tsx
+++ src/blocks/About/Component.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import RichText from '@/components/RichText'
 
 import type { AboutBlock as AboutBlockProps } from '@/payload-types'
-
-import { CMSLink } from '../../components/Link'
+import { RenderBlocks } from '../RenderBlocks'
 
 export const AboutBlock: React.FC<AboutBlockProps> = (props) => {
-  return <>About</>
+  const { layout } = props
+  return <RenderBlocks blocks={layout} />
 }
