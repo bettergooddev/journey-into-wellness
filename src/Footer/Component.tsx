d470ca1291465c8b4504e1635a740c5d0e7b78ec
bettergooddev <dev@bettergood.agency>
2025-03-25 11:49:40 -0400

    Refactor Footer component to improve structure and styling; update data handling and layout for better presentation

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -1,34 +1,40 @@
 import { getCachedGlobal } from '@/utilities/getGlobals'
-import Link from 'next/link'
 import React from 'react'
 
-import type { Footer } from '@/payload-types'
+import type { Footer as FooterType } from '@/payload-types'
 
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 import { CMSLink } from '@/components/Link'
-import { Logo } from '@/components/Logo/Logo'
-
+import { cn } from '@/utilities/ui'
+import { appendKeys } from '@/utilities/appendKeys'
 export async function Footer() {
-  const footerData: Footer = await getCachedGlobal('footer', 1)()
+  const footerData = (await getCachedGlobal('footer', 1)()) as FooterType
 
-  const navItems = footerData?.navItems || []
+  const { body, sections } = footerData
+  const { heading, actions: actionsProp } = body
+  const actions = appendKeys(actionsProp)
 
   return (
-    <footer className="mt-auto border-t border-border bg-black text-white dark:bg-card">
-      <div className="container flex flex-col gap-8 py-8 md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
-          <Logo />
-        </Link>
-
-        <div className="flex flex-col-reverse items-start gap-4 md:flex-row md:items-center">
-          <ThemeSelector />
-          <nav className="flex flex-col gap-4 md:flex-row">
-            {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
-            })}
-          </nav>
+    <>
+      <footer className="bg-primary-50 py-16 text-left text-primary">
+        <div className="container-large grid grid-cols-2">
+          <div>
+            <h3 className="max-w-[22ch]">{heading}</h3>
+            {Array.isArray(actions) && actions.length > 0 && (
+              <ul className={cn('mt-8 flex flex-row flex-wrap gap-4')}>
+                {actions.map(({ key, link }, i) => {
+                  const { key: _, ...linkProps } = link
+                  const appearance = i === 0 ? 'primary' : 'outline'
+                  return (
+                    <li key={key} className="w-full lg:w-min">
+                      <CMSLink {...linkProps} className={cn('w-full')} appearance={appearance} />
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   )
 }
